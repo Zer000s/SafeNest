245a452a18e8e7d88b90083a588d2c8a2ae4a9b1
znach <znachok.egor@gmail.com>
2025-09-20 15:18:14 +0300

    init

---
+++ src/test/java/org/example/safenest/SafeNestApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.safenest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SafeNestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
